f55fa073c1685dc968b0c678a4034cf9a28d4633
v1nland <martin.saavedra@mail.udp.cl>
2019-10-15 00:52:12 -0300

    Added CORS. (#6)

--- arq-rest-api.go
+++ arq-rest-api.go
@@ -1,11 +1,26 @@
 package main
 import (
+    "time"
+	"github.com/gin-contrib/cors"
     "github.com/gin-gonic/gin"
     _ "github.com/go-sql-driver/mysql"
 )
 
 func StartServer() {
     router := gin.Default() // Initialize API
+
+    router.Use(cors.New(cors.Config{
+		// AllowOrigins:     []string{"http://192.168.0.14", "http://localhost"},
+		AllowMethods:     []string{"GET", "PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+        // AllowAllOrigins:  true,
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
     // Test routes
     router.GET("/Test/Database", TestDatabaseConnection)
